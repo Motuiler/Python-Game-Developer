7e72bbb5d179670541282b689792c6b461cf0741
Motuiler <ekaansh.keshri@gmail.com>
2026-02-01 12:22:17 +0100

    Add files via upload

--- Dictionary Practice.py
+++ Dictionary Practice.py
@@ -1,5 +1,5 @@
 #Count the occurrence of each vowel in the sentence given as input by the user.
-vowels={"a":0,"e":0,"i":0,"o":0,"u":0}
+'''vowels={"a":0,"e":0,"i":0,"o":0,"u":0}
 sentence=input("Enter your sentence:").lower()
 for character in sentence:
     if character in vowels:
@@ -16,10 +16,21 @@ for character in user:
             letters[character]=letters[character]+1
         else:
             letters[character]=1
-print(letters)
+print(letters)'''
 
 #Find if a given number entered by the user is a pangram or not ?
 #A pangram number is a number which contains at least one occurrence of each digit.
 pangram=int(input("Enter your numbers:"))
+numbers={"1":"1","2":"1","3":"1","4":"1","5":"1","6":"1","7":"1","8":"1","9":"1"}
+for me in pangram:
+    if me in numbers:
+        value=numbers[me]=numbers[me]+1
+        if value==numbers:
+            print("It is a Pangram")
+        else:
+            print("It isn't a Pangram")
+
+        
+
 
         

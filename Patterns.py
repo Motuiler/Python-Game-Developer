f52204b51c221c28071b200366a82cf265bec5b3
Motuiler <ekaansh.keshri@gmail.com>
2026-02-18 17:25:37 +0100

    Add files via upload

--- Patterns.py
+++ Patterns.py
@@ -4,10 +4,12 @@ HEIGHT=500
 
 def draw():
     screen.fill('black')
-    for i in range(1):
-        rectangle=Rect(100,230,55,55)
-        screen.draw.rect(rectangle,('magenta'))
-        screen.draw.circle((200,257),30,('red'))
-        screen.draw.line((300,230),(350,230),('green'))
-        screen.draw.line((300,230),(325,250),('green'))
+    rectangle=Rect(100,230,55,55)
+    screen.draw.rect(rectangle,('magenta'))
+    screen.draw.circle((200,257),30,('red'))
+    screen.draw.line((245,285),(300,285),('green'))
+    screen.draw.line((245,285),(272.5,230),('green'))  
+    screen.draw.line((300,285),(272.5,230),('green'))
+    screen.draw.line((325,285),(370,230),('blue'))      
+    screen.draw.line((325,230),(370,285),('blue'))         
 pgzrun.go()
--- Shoot the Alien.py
+++ Shoot the Alien.py
@@ -6,14 +6,21 @@ HEIGHT=500
 alien=Actor('alien')
 alien.x=random.randint(50,WIDTH-50)
 alien.y=random.randint(90,HEIGHT-50)
+result=''
+
 def draw():
     screen.fill('red')
     screen.draw.text('Shoot The Alien',center=(WIDTH/2,20),fontsize=50,color='green')
     alien.draw()
+    screen.draw.text(result,center=(WIDTH/2,50),fontsize=35,color='blue')
 
 def on_mouse_down(pos):
+    global result
     print(pos)
     if alien.collidepoint(pos):
         alien.x=random.randint(50,WIDTH-50)
         alien.y=random.randint(90,HEIGHT-50)
+        result='Good Shot!'
+    else:
+        result='You Missed!'
 pgzrun.go()

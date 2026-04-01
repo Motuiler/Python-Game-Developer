e4cebd8cc794170b84124e94e49777c78743b27a
Motuiler <ekaansh.keshri@gmail.com>
2026-04-01 16:56:28 +0200

    Add files via upload

--- quiz master.py
+++ quiz master.py
@@ -5,6 +5,10 @@ WIDTH=700
 HEIGHT=440
 
 info=[]
+totque=0
+curque=0
+question=''
+timer=10
 
 marquee=Rect(0,0,WIDTH,50)
 quebox=Rect(10,60,500,150)
@@ -18,21 +22,46 @@ skip=Rect(520,220,170,210)
 
 def draw():
     screen.fill('white')
-    screen.draw.filled_rect(marquee,'green')
+    screen.draw.filled_rect(marquee,'white')
     screen.draw.filled_rect(quebox,'red')
     screen.draw.filled_rect(timebox,'blue')
     for opt in opts:
         screen.draw.filled_rect(opt,'magenta')
     screen.draw.filled_rect(skip,'yellow')
+    screen.draw.textbox(f'Welcome to the Quiz Master! This is question {curque} of {totque}.',marquee,color='black')
+    screen.draw.textbox(question[0].strip(),quebox,color='black')
+    screen.draw.textbox(question[1].strip(),opt1,color='black')
+    screen.draw.textbox(question[2].strip(),opt2,color='black')
+    screen.draw.textbox(question[3].strip(),opt3,color='black')
+    screen.draw.textbox(question[4].strip(),opt4,color='black')
+    screen.draw.textbox('Skip',skip,color='black')
+    screen.draw.textbox(str(timer),timebox,color='black',shadow=(0.5,0.5),scolor='grey')
 
 def readfile():
-    global info
+    global info,totque
     file=open('quiz questions.txt','r')
     info=file.read().split('\n')
     file.close()
+    totque=len(info)
+
+def update():
+    marquee.x=marquee.x-3
+    if marquee.right<=0:
+        marquee.left=WIDTH
+
+def readnextque():
+    global question,curque
+    question=info.pop(0).split('|')
+    curque=curque+1
+
+def on_mouse_down(pos):
+    if skip.collidepoint(pos):
+        readnextque()
+
 
 
 readfile()
+readnextque()
 print(info)
 
 pgzrun.go()

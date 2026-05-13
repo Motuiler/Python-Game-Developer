7c7e63128df0cddf888e0ba369e68ea9e8ac39cb
Motuiler <ekaansh.keshri@gmail.com>
2026-05-13 16:59:08 +0200

    Add files via upload

--- galaga.py
+++ galaga.py
@@ -9,6 +9,9 @@ ship.center=WIDTH/2,HEIGHT-100
 bullets=[] 
 enemies=[]
 score=0
+direction=1
+gameover=False
+gameovermessage=''
 y=40
 for i in range(4):
     row=[]
@@ -26,21 +29,52 @@ print(enemies)
 
 def draw():
     screen.fill('red')
-    ship.draw()
-    for bullet in bullets:
-        bullet.draw()
-    for row in enemies:
-        for enemy in row:
-            enemy.draw()
-    screen.draw.text(f"Score:{score}",center=(WIDTH-50,10),fontsize=35,color='white')
+    if gameover==False:
+        ship.draw()
+        for bullet in bullets:
+            bullet.draw()
+        for row in enemies:
+            for enemy in row:
+                enemy.draw()
+        screen.draw.text(f"Score:{score}",center=(WIDTH-50,10),fontsize=35,color='white')
+    else:
+        screen.draw.text(gameovermessage,center=(WIDTH/2,HEIGHT/2),fontsize=50,color='white')
+        screen.draw.text(f"Your final score is:{score}",center=(WIDTH/2,HEIGHT/2+30),fontsize=35,color='white')
 
 def update():
-        if keyboard.D and ship.right<WIDTH:
-            ship.x=ship.x+10
-        elif keyboard.A and ship.left>0:
-            ship.x=ship.x-10
-        for bullet in bullets:
-             bullet.y=bullet.y-5
+    global direction,score,gameover,gameovermessage
+    movedown=False
+    if keyboard.D and ship.right<WIDTH:
+        ship.x=ship.x+10
+    elif keyboard.A and ship.left>0:
+        ship.x=ship.x-10
+    for bullet in bullets:
+        bullet.y=bullet.y-10
+        
+    if any(enemies):
+        if enemies[0][-1].x>=WIDTH or enemies[0][0].x<=0:
+            direction=direction*-1
+            movedown=True
+    else:
+        gameover=True
+        gameovermessage='Well Played!'
+    
+
+    for row in enemies:
+        for enemy in row:
+            enemy.x=enemy.x+10*direction
+            if movedown:
+                enemy.y=enemy.y+40
+    
+    for i in range(len(enemies)):
+        for enemy in enemies[i]:
+            for bullet in bullets:
+                if enemy.colliderect(bullet):
+                    bullets.remove(bullet)
+                    enemies[i].remove(enemy)
+                    score=score+1
+    
+
 
 def on_key_down(key):
     if key==keys.SPACE:

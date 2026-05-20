8dd8a9d47d4b7dfbe2621b091032f49bb8dc8ea2
Motuiler <ekaansh.keshri@gmail.com>
2026-05-20 16:57:42 +0200

    Add files via upload

--- ocean map/ocean map.py
+++ ocean map/ocean map.py
@@ -1,35 +1,44 @@
 import pgzrun
 import random
 
-WIDTH=500
-HEIGHT=500
+WIDTH=510
+HEIGHT=510
+TITLE='Ocean Map Treasure Hunt'
 
 oceans=[]
-y=40
-
+attempts=7
+gameover=False
+y=10
 for i in range(5):
     row=[]
-    x=40
+    x=10
     for u in range(5):
         ocean=Actor('ocean')
-        ocean.x=x
-        ocean.y=y
+        ocean.topleft=x,y
         row.append(ocean)
-        x=x+70
+        x=x+100
     oceans.append(row)
-    y=y+70
+    y=y+100
 treasurerow=random.randint(0,4)
 treasurecolumn=random.randint(0,4)
 
 def draw():
+    screen.fill('white')
     for row in oceans:
         for ocean in row:
             ocean.draw()
+    screen.draw.text(f'Attempts:{attempts}',(15,5),fontsize=30,color='black')
 
 def on_mouse_down(pos):
-    global treasurerow,treasurecolumn
-    if mouse.colidepoint(pos):
-        treasurerow
-        treasurecolumn
+    global gameover,attempts
+    for i in range(5):
+        for j in range(5):
+            if oceans[i][j].collidepoint(pos):
+                attempts=attempts-1
+                if i==treasurerow and j==treasurecolumn:
+                    oceans[i][j].image='treasure'
+                if attempts==0:
+                    gameover=True
+
 
 pgzrun.go()
